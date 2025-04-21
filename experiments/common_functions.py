284f18bfc7762b35f4c2f19e64cd329ddce983d9
kamucomecats <yuta223125@gmail.com>
2025-04-21 14:22:16 +0900

    add common_functions.py, edited docs files

---
+++ experiments/common_functions.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+def softmax(a):
+    c = np.max(a)
+    exp_a=np.exp(a-c)
+    sum_exp_a = np.sum(exp_a)
+    softmax_a = a
+    y = exp_a/sum_exp_a
+    return y
+
+def cross_entropy_error(x, t): #valid only for one-hot case
+    d=1e-4
+    y=-np.sum(t*np.log(x+d))
+    return y
+
+x = [5, 10, 0, 100000]
+print(softmax(x))
