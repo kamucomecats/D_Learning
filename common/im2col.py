eee8687e72d1262dd5714f7b800f92b38a23fa3a
kamucomecats <yuta223125@gmail.com>
2025-05-07 04:39:20 +0900

    Add padding function

--- common/im2col.py
+++ common/im2col.py
@@ -3,15 +3,29 @@ from numpy.lib.stride_tricks import as_strided
 
 #assumed input shape is [batch_size, channel, height, width]
 #assumed output shape is [batch_size, -1] with pad
-def im2col(X, FH, FW, strides=1, pad=0):
+def im2col(X, FH, FW, strides=1, pad=1):
+
+    X = pad_input(X, pad) #padding here
+
     H, W = X.shape
     OH = H - FH + 1
     OW = W - FW + 1
+    
     shape = (OH, OW, FH, FW)
     strides = X.strides * 2
     
     patches = as_strided(X, shape=shape, strides=strides)
     return patches.reshape(OH * OW, -1)
+
+def pad_input(x, pad):
+    H, W = x.shape
+    H_pad = H + pad * 2
+    W_pad = W + pad * 2
+    x_pad = np.zeros((H_pad, W_pad), dtype=x.dtype)
+    
+    x_pad[pad:pad+H, pad:pad+W] = x
+    print(x_pad)
+    return x_pad
 
 x = np.array([[1.0, 2.0, 7.0],
      [3.0, 5.0, 3.0],
